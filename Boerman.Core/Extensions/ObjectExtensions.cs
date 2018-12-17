be1af8834706f93364663428ddf917dc2afd08ca
Corstian Boerman <mail@corstianboerman.com>
2018-12-17 13:41:45 +0100

    Move serialization logic to a more accessible location

--- Boerman.Core/Extensions/ObjectExtensions.cs
+++ Boerman.Core/Extensions/ObjectExtensions.cs
@@ -3,6 +3,8 @@
  */
 
 using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace Boerman.Core.Extensions
 {
@@ -35,6 +37,57 @@ namespace Boerman.Core.Extensions
         public static object IfThen(this object target, Func<object, bool> predicate, object thenThis)
         {
             return predicate.Invoke(target) ? thenThis : target;
+        }
+
+        /// <summary>
+        /// Serialize an object to a byte array.
+        /// </summary>
+        /// <param name="obj">The object to serialize.</param>
+        /// <returns>A byte array. Null on exception.</returns>
+        public static byte[] Serialize(this object obj)
+        {
+            MemoryStream stream = new MemoryStream();
+            BinaryFormatter formatter = new BinaryFormatter();
+            try
+            {
+                formatter.Serialize(stream, obj);
+                return stream.ToArray();
+            }
+            catch
+            {
+                return null;
+            }
+            finally
+            {
+                stream.Close();
+            }
+        }
+
+        /// <summary>
+        /// Deserialize an byte array to a specific object.
+        /// </summary>
+        /// <param name="buffer">The byte array to deserialize</param>
+        /// <returns>The object T to serialize to. Null on exception</returns>
+        public static T Deserialize<T>(this byte[] buffer) where T : class
+        {
+            var formatter = new BinaryFormatter();
+            var stream = new MemoryStream(buffer)
+            {
+                Position = 0
+            };
+
+            try
+            {
+                return formatter.Deserialize(stream) as T;
+            }
+            catch
+            {
+                return null;
+            }
+            finally
+            {
+                stream.Close();
+            }
         }
     }
 }
