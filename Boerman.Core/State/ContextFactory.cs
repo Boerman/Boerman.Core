7983292ebc184e166be2b7f6cd8647ea327bde08
Corstian Boerman <mail@corstianboerman.com>
2018-12-17 13:12:22 +0100

    Add comments to the contextFactory

--- Boerman.Core/State/ContextFactory.cs
+++ Boerman.Core/State/ContextFactory.cs
@@ -5,16 +5,24 @@ using System.Timers;
 
 namespace Boerman.Core.State
 {
+    /// <summary>
+    /// The abstract <see cref="ContextFactory{T}"/> class helps creating and keeping track of multiple context instances.
+    /// </summary>
+    /// <typeparam name="T">The type of the <see cref="BaseContext"/> to use with this factory.</typeparam>
     public abstract class ContextFactory<T> where T : BaseContext
     {
         private readonly ConcurrentDictionary<Guid, T> _contextInstances = new ConcurrentDictionary<Guid, T>();
         
+        /// <summary>
+        /// The abstract constructor for the <see cref="ContextFactory{T}"/> class.
+        /// </summary>
+        /// <param name="timeout">The timeout after which to unreference a context if there is no activity.</param>
         protected ContextFactory(double timeout = 0)
         {
             if (timeout != 0)
             {
                 // Please note that this code is just a quick fix to remove unneeded context instances over a while.
-                _timer = new Timer(1000);
+                _timer = new Timer(1000);   // We check for context instances when they were last active with this interval.
                 if (timeout > 0) timeout = -timeout;
 
                 _timer.Elapsed += (sender, args) =>
@@ -39,21 +47,31 @@ namespace Boerman.Core.State
             return RetrieveContext(func) != null;
         }
 
+        /// <summary>
+        /// Retrieve a specific context based on a given predicate.
+        /// </summary>
+        /// <param name="func">The predicate to evaluate</param>
+        /// <returns>A single context, or null</returns>
         public T RetrieveContext(Func<T, bool> func)
         {
-            var instances = _contextInstances.Values.Where(func.Invoke).ToList();
-
-            if (instances.Count > 1)
-                throw new ArgumentException("Predicate returns more then one context instance", nameof(func));
-
-            return instances.FirstOrDefault();
+            return _contextInstances.Values.SingleOrDefault(func.Invoke);
         }
 
+        /// <summary>
+        /// Retrieve a context based on the identifier for said context
+        /// </summary>
+        /// <param name="guid">The identifier for the specific context.</param>
+        /// <returns>Context or null</returns>
         public T RetrieveContext(Guid guid)
         {
             return _contextInstances[guid];
         }
 
+        /// <summary>
+        /// Add a context to be tracked by the factory.
+        /// </summary>
+        /// <param name="context">The context to track</param>
+        /// <returns>The identifier by which this context is tracked</returns>
         public Guid AddContext(T context)
         {
             var guid = Guid.NewGuid();
@@ -63,6 +81,10 @@ namespace Boerman.Core.State
             throw new Exception("Could not add context instance");
         }
 
+        /// <summary>
+        /// Remove a context by its identifier
+        /// </summary>
+        /// <param name="guid">The identifier for the context to remove</param>
         public void RemoveContext(Guid guid)
         {
             _contextInstances.TryRemove(guid, out T context);
